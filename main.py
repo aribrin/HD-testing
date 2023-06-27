82382fcb78603a079bb309bda0c8caaaf2d45a20
aribrin <114630291+aribrin@users.noreply.github.com>
2023-06-27 13:35:38 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import torch
+from torchvision import datasets, transforms
+
+class HD():
+    def __init__(self):
+        
+       
+    def forward(self, x):
+       
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    
+
+def test(model, device, test_loader):
+    
+
+def main():
+    
